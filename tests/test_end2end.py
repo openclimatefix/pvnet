3790ac258e4ea86cfdf5bc2235a5674be058bfcf
James Fulton <djamesfulton@yahoo.co.uk>
2025-09-01 10:51:12 +0000

    remove fast_dev_run

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -3,14 +3,15 @@ from pvnet.data import  UKRegionalStreamedDataModule
 from pvnet.training.lightning_module import PVNetLightningModule
 from pvnet.optimizers import EmbAdamWReduceLROnPlateau
 
+
 def test_model_trainer_fit(session_tmp_path, uk_data_config_path, late_fusion_model):
     """Test end-to-end training."""
 
     datamodule = UKRegionalStreamedDataModule(
         configuration=uk_data_config_path,
         batch_size=2,
-        num_workers=0,
-        prefetch_factor=None,
+        num_workers=2,
+        prefetch_factor=1,
         dataset_pickle_dir=f"{session_tmp_path}/dataset_pickles"
     )
 
@@ -20,5 +21,5 @@ def test_model_trainer_fit(session_tmp_path, uk_data_config_path, late_fusion_mo
     )
 
     # Train the model for one batch
-    trainer = lightning.Trainer(fast_dev_run=True, accelerator="cpu")
+    trainer = lightning.Trainer(max_epochs=2, accelerator="cpu")
     trainer.fit(model=ligtning_model, datamodule=datamodule)
